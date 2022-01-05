1b5a1fc38127cc40f6a574a2871b17f3cb04c2fe
wassimtaktak <wassimtaktak32@gmail.com>
2022-01-05 21:26:41 +0100

    e

--- src/election/UseHome.java
+++ src/election/UseHome.java
@@ -8,7 +8,7 @@ import java.awt.event.ActionEvent;
 public class UseHome extends JFrame {
 	public UseHome() {
 		getContentPane().setLayout(null);
-		setSize(469,460);
+		setSize(511,460);
 		JButton btnNewButton = new JButton("Gestion des  candidats ");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -38,6 +38,11 @@ public class UseHome extends JFrame {
 		btnNewButton_2.setFont(new Font("Tahoma", Font.BOLD, 15));
 		btnNewButton_2.setBounds(100, 309, 248, 69);
 		getContentPane().add(btnNewButton_2);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(UseHome.class.getResource("/img/wallpapertip_website-wallpaper-backgrounds_1963020.jpg")));
+		lblNewLabel.setBounds(0, 0, 497, 423);
+		getContentPane().add(lblNewLabel);
 		
 	}
 }
